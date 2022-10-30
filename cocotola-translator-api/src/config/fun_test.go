6377c05c66cd04e018aabd21ac6fa42fcc33d218
pecolynx <pecolynx@gmail.com>
2022-10-30 19:56:37 +0900

    fix path

--- cocotola-translator-api/src/config/fun_test.go
+++ cocotola-translator-api/src/config/fun_test.go
@@ -1,6 +1,10 @@
-package errors
+package config_test
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/kujilabo/cocotola/cocotola-api-translator/src/config"
+)
 
 func TestAbc(t *testing.T) {
 	tests := []struct {
@@ -16,7 +20,7 @@ func TestAbc(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := Abc(tt.args); got != tt.want {
+			if got := config.Abc(tt.args); got != tt.want {
 				t.Errorf("Abc() = %v, want %v", got, tt.want)
 			}
 		})
