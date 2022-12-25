27820cae13eb5d4ee2b10b93c72eebe29156d710
pecolynx <pecolynx@gmail.com>
2022-12-25 17:07:35 +0900

    cloudbuild

--- cocotola-api/src/app/gateway/repository_factory.go
+++ cocotola-api/src/app/gateway/repository_factory.go
@@ -109,25 +109,3 @@ func (f *repositoryFactory) NewUserRepositoryFactory(ctx context.Context) (userS
 }
 
 type RepositoryFactoryFunc func(ctx context.Context, db *gorm.DB) (service.RepositoryFactory, error)
-
-type transaction struct {
-	db  *gorm.DB
-	rff RepositoryFactoryFunc
-}
-
-func NewTransaction(db *gorm.DB, rff RepositoryFactoryFunc) (service.Transaction, error) {
-	return &transaction{
-		db:  db,
-		rff: rff,
-	}, nil
-}
-
-func (t *transaction) Do(ctx context.Context, fn func(rf service.RepositoryFactory) error) error {
-	return t.db.Transaction(func(tx *gorm.DB) error { // nolint:wrapcheck
-		rf, err := t.rff(ctx, tx)
-		if err != nil {
-			return liberrors.Errorf(". err: %w", err)
-		}
-		return fn(rf)
-	})
-}
