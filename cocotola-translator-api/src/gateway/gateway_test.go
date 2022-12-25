deaa0fb4b0ef4b3decda418f345776d33d818d32
pecolynx <pecolynx@gmail.com>
2022-12-25 14:01:38 +0900

    fix translator

--- cocotola-translator-api/src/gateway/gateway_test.go
+++ cocotola-translator-api/src/gateway/gateway_test.go
@@ -1,9 +1,6 @@
 package gateway_test
 
 import (
-	"context"
-
-	"github.com/kujilabo/cocotola/cocotola-translator-api/src/service"
 	"github.com/kujilabo/cocotola/cocotola-translator-api/src/sqls"
 	testlibG "github.com/kujilabo/cocotola/test-lib/gateway"
 )
@@ -11,18 +8,4 @@ import (
 func init() {
 	testlibG.InitMySQL(sqls.SQL, "127.0.0.1", 3317)
 	testlibG.InitSQLite(sqls.SQL)
-}
-
-type transaction struct {
-	rf service.RepositoryFactory
-}
-
-func newTransaction(rf service.RepositoryFactory) service.Transaction {
-	return &transaction{
-		rf: rf,
-	}
-}
-
-func (t *transaction) Do(ctx context.Context, fn func(rf service.RepositoryFactory) error) error {
-	return fn(t.rf)
 }
